3caab060cf3d8c58c96d94544217774d6869f268
barisozdmiir <wpbaris@gmail.com>
2024-04-26 20:44:35 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {FiShoppingBag} from 'react-icons/fi'
 import { SidebarContext } from '../contexts/SidebarContext'
+import { CartContext } from '../contexts/CartContext';
+import { Link } from 'react-router-dom';
+import Logo from '../img/logo.svg'
 
 function Header() {
-  const {isOpen,setIsOpen} = useContext(SidebarContext)
+  const [isActive,setIsActive] = useState(false);
+  const {isOpen,setIsOpen} = useContext(SidebarContext);
+  const {itemAmount} = useContext(CartContext);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    })
+  })
   return (
-    <header className='bg-green-300'>
-      <div>
-        Header
-      </div>
-      <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
-        <FiShoppingBag className='text-2xl' />
+    <header className={`${isActive ? 'bg-white shadow-md' : 'bg-none'} fixed w-full z-10 transition-all`}>
+      <div className='flex container mx-auto items-center justify-between h-full'>
+        <Link>
+          <div>
+            <img className='w-[40px]' src={Logo} alt="" />
+          </div>
+        </Link>
+        <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative '>
+          <FiShoppingBag className='text-2xl' />
+         <div className='bg-red-600 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>{itemAmount}</div>
+        </div>
+
       </div>
     </header>
   )
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,26 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import CartItem from '../components/CartItem';
 
 export const CartContext = createContext();
 
 function CartProvider({children}) {
   const [cart, setCart] = useState([]);
+  const [itemAmount, setItemAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount;
+    },0);
+    setTotal(total)
+  });
+  useEffect(() => {
+    if (cart) {
+      const amount = cart.reduce((accumulator, currentItem)=> {
+        return accumulator + currentItem.amount;
+      },0);
+      setItemAmount(amount);
+    }
+  },[cart]);
   const addToCart = (product, id) => {
     const newItem = {...product, amount: 1};
     const cartItem = cart.find(item => {
@@ -22,8 +39,40 @@ function CartProvider({children}) {
       setCart([...cart, newItem]);
     }
   };
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => {
+      return item.id !== id;
+    })
+    setCart(newCart);
+  }
+  const clearCart = () => {
+    setCart([])
+  };
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    addToCart(cartItem, id);
+  };
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    console.log(cartItem)
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount - 1};
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } if (cartItem.amount < 2) {
+      removeFromCart(id);
+    }
+  }
+
   return (
-    <CartContext.Provider value={{cart, addToCart}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount, itemAmount, total}}>{children}</CartContext.Provider>
   )
 }
 
